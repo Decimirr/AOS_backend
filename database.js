8d86b045b1331d551c2c8cc4003c8e08cdfce2fc
Decimirr <kim0724@snu.ac.kr>
2022-07-13 01:51:47 +0900

    7/13

--- database.js
+++ database.js
@@ -1,6 +1,17 @@
 const mysql = require('mysql')
 const fs = require('fs')
 
-const con = mysql.createPool({host:"aos-database.mysql.database.azure.com", user:"Decimirr", password:process.env.database_password, database:'aos', port:3306, ssl:{rejectUnauthorized: false, ca:fs.readFileSync("./secret/BaltimoreCyberTrustRoot.crt.pem")}, multipleStatements: true});
+const con = mysql.createPool({
+  host:"aos-database.mysql.database.azure.com",
+  user:"Decimirr",
+  password:process.env.database_password,
+  database:'aos',
+  port:3306,
+  ssl:{rejectUnauthorized: false, ca:fs.readFileSync("./secret/BaltimoreCyberTrustRoot.crt.pem")},
+  multipleStatements: true,
+  connectTimeout  : 60 * 60 * 1000,
+  acquireTimeout  : 60 * 60 * 1000,
+  timeout         : 60 * 60 * 1000,
+});
 
 module.exports = con
